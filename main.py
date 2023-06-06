2f33fc7b2ccd2a70dcbf365cc1034ff515ae8e1d
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 22:51:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#myName = input("whats's your name?: ")
+#if myName == "David":
+#  print("Welcome Dude!")
+#  print("You're just the baldest dude I've ever seen")
+#else:
+#  print("Who on earth are you?!")
+
+#Common errors
+#catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+#if catsOrDogs == "cats":
+#  print("Meow")
+#else:
+#  print("Woof!")
+
+#Fix my code
+#catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+#if catsOrDogs == "cat":
+#  print("Meow")
+#else:
+#  print("Woof")
+
+#drink = input("Do you prefer coffee or tea?")
+#if drink == "coffee":
+#  print("Tea is better.")
+#else:
+#  print("Excellent choice.")
+
+#Challenge
+print("Which Lost Highway charachter are you? ")
+print("---")
+prosti = input("Have you ever practiced prostitution? ")
+if prosti == "no":
+  print("So you are not Alice Wakefield")
+  print()
+else:
+  print("Hello! Alice")
+  print()
+
+killer = input("Have you ever killed your wife before? ")
+if killer == "yes":
+  print("Fred, please don't do it again")
+  print()
+else:
+  print("You are not Fred!.")
+  print()
+
+ghost = input("Are you able to be in two places at a time right? ")
+if ghost == "yes":
+  print("You are the frightening Mystery Man!")
+  print()
+else:
+  print("Sadly, you are not the frightening Mystery Man")
+  print()
+
+print("""
+
+If you answered no to all of the previous questions, you are quite boring and might be something like an extra.
+
+""")
